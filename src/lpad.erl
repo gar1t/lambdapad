4bec4dae988167932efa7d4e808e52528ec87406
Garrett Smith <g@rre.tt>
2014-08-11 08:46:35 -0500

    Copyright banners and whitespace

--- src/lpad.erl
+++ src/lpad.erl
@@ -3,9 +3,9 @@
 %%% Licensed under the Apache License, Version 2.0 (the "License");
 %%% you may not use this file except in compliance with the License.
 %%% You may obtain a copy of the License at
-%%% 
+%%%
 %%%     http://www.apache.org/licenses/LICENSE-2.0
-%%% 
+%%%
 %%% Unless required by applicable law or agreed to in writing, software
 %%% distributed under the License is distributed on an "AS IS" BASIS,
 %%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -57,7 +57,7 @@ index_src_name() ->
 compile_index(Src) ->
     CompileOpts =
         [export_all,
-         return_errors, 
+         return_errors,
          binary,
          {i, lpad_include_dir()}],
     handle_index_compile(compile:file(Src, CompileOpts), Src).
