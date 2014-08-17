d76a4353e6dc8b1537a77216b2c4eab2765e772a
Garrett Smith <g@rre.tt>
2014-08-17 13:47:32 -0500

    More error handling

--- src/lpad_event.erl
+++ src/lpad_event.erl
@@ -53,6 +53,8 @@ handle_error({{template_compile, _, Error}, _}) ->
     handle_template_compile_error(Error);
 handle_error({{index_compile, _, Error, _}, _}) ->
     handle_index_compile_error(Error);
+handle_error({Reason, [{_, _, _, [{file, File}, {line, Line}]}|_]}) ->
+    handle_general_error(Reason, File, Line);
 handle_error({{_, File, {Line, erl_parse, Msg}}, _}) ->
     log_line_error(File, Line, Msg);
 handle_error(Other) ->
@@ -77,18 +79,20 @@ log_index_error({File, Errors}) ->
 
 log_index_error(File, {Line, erl_parse, Msg}) ->
     log_line_error(File, Line, Msg);
-log_index_error(File, {Line, erl_lint, {undefined_function, Fun}}) ->
-    log_line_error(File, Line, format_undef_error(Fun));
-log_index_error(File, {Line, erl_lint, {unbound_var, Var}}) ->
-    log_line_error(File, Line, format_unbound_error(Var));
+log_index_error(File, {Line, erl_lint, Reason}) ->
+    log_line_error(File, Line, format_error_reason(Reason));
 log_index_error(File, Other) ->
     log_error(banner("INDEX ERROR"), [{File, Other}]).
 
-format_undef_error({Name, Arity}) ->
-    io_lib:format("undefined function ~s/~b", [Name, Arity]).
+handle_general_error(Reason, File, Line) ->
+    log_line_error(File, Line, format_error_reason(Reason)).
 
-format_unbound_error(Var) ->
-    io_lib:format("unbound variable ~s", [Var]).
+format_error_reason({undefined_function, {Name, Arity}}) ->
+    io_lib:format("undefined function ~s/~b", [Name, Arity]);
+format_error_reason({unbound_var, Var}) ->
+    io_lib:format("unbound variable ~s", [Var]);
+format_error_reason(Other) ->
+    io_lib:format("~p", [Other]).
 
 log_line_error(File, Line, Msg) ->
     log_error("~s:~b: ~s~n", [File, Line, Msg]).
